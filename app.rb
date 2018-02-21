38ff9da2957f49878f51fabb224027ed4b5a06c0
Anthony Harvey <anthonygharvey@gmail.com>
2018-02-21 20:02:44 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 class App < Sinatra::Base
-
-  # Define your GET '/' route below and respond with "Hello, World!"
-
-
+  resp = Rack::Response.new
+  get '/' do
+    resp.status = 200
+    "Hello, World!"
+  end
 end
